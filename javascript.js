f3c1bfb1736f606879013ce95c0b558662f94724
Lucas Eduardo Campolino <campolino.l@gmail.com>
2022-08-04 02:12:16 -0300

    Canvas create and has iteraction with the user

--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
 const canvas = document.querySelector('.canvas');
+const reset = document.querySelector('#reset-canvas');
 
+// Create canvas grids
 function createGrid(size) {
   for(let i = 0; i < (size * size); i++) {
     let grid = document.createElement('div');
@@ -9,14 +11,33 @@ function createGrid(size) {
   gridLayout(size);
 }
 
+// Set width and height of the grids to fit the canvas
 function gridLayout(size) {
   console.log(canvas.childElementCount);
   let squares = Array.from(document.querySelectorAll('.square'));
+  
   squares.forEach(square => {
-    square.style.backgroundColor = 'black';
     square.style.width = `calc(600px / ${size})`;
     square.style.height = `calc(600px / ${size})`;
   });
+
+  addEvents();
 }
 
+// Listenning to when mouse is over the grid than change it's color
+function addEvents() {
+  let squares = Array.from(document.querySelectorAll('.square'));
+
+  squares.forEach(square => {
+    square.addEventListener('mouseover', paint);
+  })
+}
+
+// This generate a random HEX color
+function paint() {
+  let colorGenerator = Math.floor(Math.random()*16777215).toString(16);
+  this.style.backgroundColor = "#" + colorGenerator;
+}
+
+// This call the function that create the grids
 createGrid(16);
