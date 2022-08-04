a42b3787cde9cccf1af70b2cdd614b098dd49bf7
Lucas Eduardo Campolino <campolino.l@gmail.com>
2022-08-04 02:59:21 -0300

    Add option to resize canvas

--- javascript.js
+++ javascript.js
@@ -1,5 +1,9 @@
 const canvas = document.querySelector('.canvas');
 const reset = document.querySelector('#reset-canvas');
+const resize = document.querySelector('#change-size');
+const sliderValue = document.querySelector('.slider');
+const sliderText = document.querySelector('.sliderText');
+sliderText.textContent = sliderValue.value;
 
 // Create canvas grids
 function createGrid(size) {
@@ -13,10 +17,10 @@ function createGrid(size) {
 
 // Set width and height of the grids to fit the canvas
 function gridLayout(size) {
-  console.log(canvas.childElementCount);
   let squares = Array.from(document.querySelectorAll('.square'));
   
   squares.forEach(square => {
+    square.style.backgroundColor = 'white';
     square.style.width = `calc(600px / ${size})`;
     square.style.height = `calc(600px / ${size})`;
   });
@@ -46,11 +50,24 @@ function wipeCanvas() {
   squares.forEach(square => square.remove());
 
   // Call the function to recreate grids
-  createGrid(16);
+  createGrid(sliderValue.value);
+}
+
+// Resize the canvas
+function changeSize() {
+  wipeCanvas();
 }
 
 // This call the function that create the grids
-createGrid(16);
+createGrid(sliderValue.value);
+
+// Call function that resize canvas by the value of the slider
+resize.addEventListener('click', changeSize);
+
+// Show the value of the slider
+sliderValue.addEventListener('input', function() {
+  sliderText.textContent = `${sliderValue.value}`;
+});
 
 // Listenning for when the user click to reset canvas
 reset.addEventListener('click', wipeCanvas);
